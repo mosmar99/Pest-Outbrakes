9bb5d4614439f4b21bfb52b60cf1c41d2ddef575
Isac Paulsson <isac.97@live.se>
2025-02-17 12:46:31 +0100

    Added date and station type filtering on smhi station

--- smhi_api.py
+++ smhi_api.py
@@ -20,15 +20,36 @@ def fetch_data_on_key(endpoint, json_key):
         return None
 
 def get_all_parameters():
-    return fetch_data_on_key(SMHI_URI["all_parameters"], "resource")
+    df = fetch_data_on_key(SMHI_URI["all_parameters"], "resource")
+    df = df.drop(['link', 'geoBox'], axis=1)
+    return df
 
-def get_all_stations_on_parameter_id(param_id="19"):
+def get_all_stations_on_parameter_id(param_id="19", measuringStations=None, from_date=None, to_date=None):
     df = fetch_data_on_key(f'{SMHI_URI["all_stations"]}/{param_id}.json', "station")
+
     gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.longitude, df.latitude), crs="EPSG:4326")
+    gdf = gdf.drop(['latitude','longitude', 'summary', 'link', 'id'], axis=1)
+
+    gdf['updated'] = pd.to_datetime(gdf['updated'], unit="ms")
+    gdf['from'] = pd.to_datetime(gdf['from'], unit="ms")
+    gdf['to'] = pd.to_datetime(gdf['to'], unit="ms")
+
+    if measuringStations:
+        measuringStations_mask = gdf["measuringStations"] == measuringStations
+        gdf = gdf[measuringStations_mask]
+
+    if from_date:
+        from_mask = gdf['from'] < pd.to_datetime(from_date)
+        gdf = gdf[from_mask]
+    
+    if to_date:
+        to_mask = gdf['to'] > pd.to_datetime(to_date)
+        gdf = gdf[to_mask]
+    
     return gdf
 
 if __name__ == "__main__":
-    # df = get_all_parameters()
-    # print(df)
+    df = get_all_parameters()
+    print(df)
     gdf = get_all_stations_on_parameter_id()
     print(gdf)
--- test_station.py
+++ test_station.py
@@ -1,5 +1,6 @@
 import api
 import smhi_api
+import datetime as dt
 import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
@@ -7,32 +8,42 @@ from shapely.geometry import LineString
 
 
 if __name__ == "__main__":
-    gradings_df = api.get_gradings(from_date="2019-03-01", crop="Höstvete", pest="svartpricksjuka")
-    # gradings_df = api.sweref99tm_to_wgs84(gradings_df)
+    from_date = dt.datetime(2019,3,1)
+    to_date = dt.datetime(2019,11,22)
+
+    gradings_df = api.get_gradings(from_date=(from_date), to_date=str(to_date))
     jbv_gdf = gpd.GeoDataFrame(gradings_df, geometry=gpd.points_from_xy(gradings_df.longitud, gradings_df.latitud), crs="EPSG:3006")
     jbv_gdf["geometry"] = jbv_gdf["geometry"].to_crs("EPSG:4326")
 
-    smhi_stations_gdf = smhi_api.get_all_stations_on_parameter_id()
+    smhi_stations_gdf = smhi_api.get_all_stations_on_parameter_id(measuringStations="CORE", from_date=from_date, to_date=to_date)
 
-
+    # Convert to estimated UTM CRS to achieve accuracy in distances
     utm_crs = jbv_gdf.estimate_utm_crs()
     jbv_gdf = jbv_gdf.to_crs(utm_crs)
     smhi_stations_gdf = smhi_stations_gdf.to_crs(utm_crs)
+    # Keep station point for plotting
     smhi_stations_gdf["station_loc"] = smhi_stations_gdf["geometry"]
 
+    # Perform Spatial join by nearest neighbour
     jbv_gdf = jbv_gdf.sjoin_nearest(smhi_stations_gdf, how="left")
 
-    # print(f"JBV CRS: {jbv_gdf.crs}, SMHI CRS: {smhi_stations_gdf.crs}")
-    # print("\nGradings DataFrame:")
-    # print(jbv_gdf.head(7))
-    print("\nStations DataFrame:")
-    print(smhi_stations_gdf.head(7))
+    print(pd.unique(jbv_gdf["key"]))
 
-    jbv_gdf['connection'] = jbv_gdf.apply(
-    lambda row: LineString([row['geometry'], row['station_loc']]), axis=1)
+    # print(f"JBV CRS: {jbv_gdf.crs}, SMHI CRS: {smhi_stations_gdf.crs}")
+    print("\nGradings DataFrame:")
+    print(jbv_gdf.head(7))
+    # print("\nStations DataFrame:")
+    # print(smhi_stations_gdf.head(7))
+
+    def create_connection(row, l_key, r_key):
+        if row[l_key].is_empty or row[r_key].is_empty:
+            return None  # or return LineString() to create an empty LineString
+        else:
+            return LineString([row[l_key], row[r_key]])
+    
+    jbv_gdf['connection'] = jbv_gdf.apply(lambda row: create_connection(row, l_key='geometry', r_key='station_loc'), axis=1)
 
     connections = gpd.GeoDataFrame(jbv_gdf['connection'], geometry='connection', crs=utm_crs)
-
 
     # Plot the results
     fig, ax = plt.subplots(figsize=(10, 10))
