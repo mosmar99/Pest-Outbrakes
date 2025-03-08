d82111eecf487762628a6a0ae40928248a1c883b
Isac Paulsson <isac.97@live.se>
2025-03-08 21:15:58 +0100

    Updated smhi processing to get data from multiple closest stations and take an average, possibility to set max distance

--- data.py
+++ data.py
@@ -6,6 +6,7 @@ import data_processing.smhi_processing as smhi_processing
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import geopandas as gpd
 
 pd.set_option('future.no_silent_downcasting', True)
 
@@ -21,6 +22,16 @@ if __name__ == '__main__':
     wanted_features = ['ekologisk', 'groda', 'skadegorare', 'graderingsdatum', 'utvecklingsstadium', 'varde', 'latitud', 'longitud', 'Series_id']
     data_df = jbv_process.feature_extraction(data_json, wanted_features) 
     print('JBV FEATURES:', data_df.shape)
+
+    agg_dict = {'ekologisk': 'first',
+                'groda': 'first',
+                'skadegorare': 'first',
+                'utvecklingsstadium': 'mean',
+                'varde': 'mean',
+                'latitud': 'first',
+                'longitud': 'first'}
+
+    data_df = data_df.groupby(['Series_id', 'graderingsdatum']).agg(agg_dict).reset_index()
 
     # Aggregate JBV data
 
@@ -45,14 +56,16 @@ if __name__ == '__main__':
     print('DROPPED SHORT/UNCOMMON SERIES', data_df.shape) 
 
     # Interpolate JBV data
-    data_df = jbv_process.interpolate_jbv_data(data_df, method='linear')
-    print('JBV INTERPOLATED:', data_df.shape)
+    # data_df = jbv_process.interpolate_jbv_data(data_df, method='linear')
+    # print('JBV INTERPOLATED:', data_df.shape)
 
     # Clean and convert coordinates
     data_df = jbv_process.clean_coordinate_format(data_df)
     data_gdf = jbv_process.sweref99tm_to_wgs84(data_df)
     data_gdf = jbv_process.remove_outside_sweden_coordinates(data_gdf)
     print('COORDS CLEANED:', data_gdf.shape)
+
+    data_gdf = data_gdf.dropna()
 
     # Get SMHI Data
     TEMP = '2'
@@ -62,15 +75,12 @@ if __name__ == '__main__':
     HUMIDITY = '6'
     AIR_PREASSUE = '9'
     LONG_WAVE_IRR = '24'
-    # WIND_SPEED = '4'
-    
-    params = [(TEMP, 'mean'),
+    WIND_SPEED = '4'
+
+    params = [(TEMP, 'mean'), #(TEMP, 'mean')
               (RAIN, 'sum'),
-              (SUN, 'sum'),
               (DEWPOINT, 'mean'),
-              (HUMIDITY, 'mean'),
-              (AIR_PREASSUE, 'mean'),
-              (LONG_WAVE_IRR, 'mean')]
+              (HUMIDITY, 'mean')]
 
     data_gdf = smhi_processing.gather_weather_data(
         data_gdf,
@@ -79,15 +89,21 @@ if __name__ == '__main__':
         smhi_api.get_station_data_on_key_param,
         from_date,
         to_date,
-        aggregation_rule='D')
+        weekly=True)
     
     print('SMHI GATHERED:', data_gdf.shape)
 
     # Save as pickle
-    data_gdf.to_pickle("test_out.pkl")
+    data_gdf.to_pickle("test_out_weekly.pkl")
 
     # Save data as test_out.pkl
     # can be read as follows
 
     # data_df = pd.read_pickle("test_out.pkl")
     # data_gdf = gpd.GeoDataFrame(data_df)
+    # print(data_gdf.columns)
+
+    print(data_gdf[data_gdf['Series_id'] == 22])
+    print(data_gdf.shape)
+    data_gdf = data_gdf.dropna()
+    print(data_gdf.shape)
